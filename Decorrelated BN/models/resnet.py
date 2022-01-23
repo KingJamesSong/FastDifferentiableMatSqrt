e3faa49cc64024fb3f0ce67e0460001f656bb8ed
YueSong <songyue19960927@gmail.com>
2022-01-23 13:49:50 +0100

    Update resnet.py

--- Decorrelated BN/models/resnet.py
+++ Decorrelated BN/models/resnet.py
@@ -174,7 +174,7 @@ class ResNet(nn.Module):
                              "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
         self.groups = groups
         self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = norm_layer(self.inplanes, affine=True)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
